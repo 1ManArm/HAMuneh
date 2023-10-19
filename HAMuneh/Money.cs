9c43c078336ab197d52e52c8b2e7a2c1162c2d55
Сергей <sergey.emelyanov.97@mail.ru>
2023-10-19 18:02:44 +0500

    Добавьте файлы проекта.

---
+++ HAMuneh/Money.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAMuneh
+{
+    internal class Money
+    {
+
+        private int rubles;
+        private int kopeks;
+
+        public Money(int rubles, int kopeks)
+        {
+            if (rubles < 0 || kopeks < 0)
+            {
+                throw new ArgumentException("Счет не может быть отрицательным");
+            }
+
+            this.rubles = rubles;
+            this.kopeks = kopeks;
+        }
+
+        public int Rubles
+        {
+            get { return rubles; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Счет не может быть отрицательным");
+                }
+
+                rubles = value;
+            }
+        }
+
+        public int Kopeks
+        {
+            get { return kopeks; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Счет не может быть отрицательным");
+                }
+
+                kopeks = value;
+            }
+        }
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            int totalKopeks = (m1.rubles * 100 + m1.kopeks) + (m2.rubles * 100 + m2.kopeks);
+            return new Money(totalKopeks / 100, totalKopeks % 100);
+        }
+
+        public static Money operator -(Money m1, Money m2)
+        {
+            int totalKopeks = (m1.rubles * 100 + m1.kopeks) - (m2.rubles * 100 + m2.kopeks);
+
+            if (totalKopeks < 0)
+            {
+                throw new Exception("Банкрот");
+            }
+
+            return new Money(totalKopeks / 100, totalKopeks % 100);
+        }
+
+        public static Money operator /(Money m, int divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            int totalKopeks = (m.rubles * 100 + m.kopeks) / divisor;
+            return new Money(totalKopeks / 100, totalKopeks % 100);
+        }
+
+        public static Money operator *(Money m, int multiplier)
+        {
+            if (multiplier < 0)
+            {
+                throw new ArgumentException("Множитель не может быть отрицательным");
+            }
+
+            int totalKopeks = (m.rubles * 100 + m.kopeks) * multiplier;
+            return new Money(totalKopeks / 100, totalKopeks % 100);
+        }
+
+        public static Money operator ++(Money m)
+        {
+            return new Money(m.rubles, m.kopeks + 1);
+        }
+
+        public static Money operator --(Money m)
+        {
+            if (m.kopeks == 0)
+            {
+                throw new Exception("Банкрот");
+            }
+
+            return new Money(m.rubles, m.kopeks - 1);
+        }
+
+        public static bool operator <(Money m1, Money m2)
+        {
+            int totalKopeks1 = m1.rubles * 100 + m1.kopeks;
+            int totalKopeks2 = m2.rubles * 100 + m2.kopeks;
+            return totalKopeks1 < totalKopeks2;
+        }
+
+        public static bool operator >(Money m1, Money m2)
+        {
+            int totalKopeks1 = m1.rubles * 100 + m1.kopeks;
+            int totalKopeks2 = m2.rubles * 100 + m2.kopeks;
+            return totalKopeks1 > totalKopeks2;
+        }
+
+        public static bool operator ==(Money m1, Money m2)
+        {
+            int totalKopeks1 = m1.rubles * 100 + m1.kopeks;
+            int totalKopeks2 = m2.rubles * 100 + m2.kopeks;
+            return totalKopeks1 == totalKopeks2;
+        }
+
+        public static bool operator !=(Money m1, Money m2)
+        {
+            int totalKopeks1 = m1.rubles * 100 + m1.kopeks;
+            int totalKopeks2 = m2.rubles * 100 + m2.kopeks;
+            return totalKopeks1 != totalKopeks2;
+        }
+
+        public override string ToString()
+        {
+            return $"{rubles} rubles {kopeks} kopeks";
+        }
+    }
+}
---
+++ HAMuneh/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace HAMuneh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm here for muneh! MUNEH!");
+            Console.WriteLine();
+            try
+            {
+                Money money1 = new Money(10, 50);
+                Money money2 = new Money(5, 75);
+
+                Console.WriteLine($"Счет 1: {money1.ToString()}");
+                Console.WriteLine($"Счет 2: {money2.ToString()}");
+
+                Money sum = money1 + money2;
+                Console.WriteLine($"Сумма: {sum.ToString()}");
+
+                Money difference = money1 - money2;
+                Console.WriteLine($"Разность: {difference.ToString()}");
+
+                Money quotient = money1 / 2;
+                Console.WriteLine($"Коэффициент: {quotient.ToString()}");
+
+                Money product = money2 * 3;
+                Console.WriteLine($"Рез-т умножения: {product.ToString()}");
+
+                Money increment = ++money1;
+                Console.WriteLine($"Инкремент: {increment.ToString()}");
+
+                Money decrement = --money2;
+                Console.WriteLine($"Декремент: {decrement.ToString()}");
+
+                bool isLess = money1 < money2;
+                Console.WriteLine($"Счет 1 < Счет 2: {isLess}");
+
+                bool isGreater = money1 > money2;
+                Console.WriteLine($"Счет 1 > Счет 2: {isGreater}");
+
+                bool isEqual = money1 == money2;
+                Console.WriteLine($"Счет 1 == Счет2: {isEqual}");
+
+                bool isNotEqual = money1 != money2;
+                Console.WriteLine($"Счет 1 != Счет 2: {isNotEqual}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
